0e6ad37d76b1b3660bcbb15f5523b826be1b02a2
mmondics <46940343+mmondics@users.noreply.github.com>
2020-09-04 09:41:37 -0400

    Update 1-build.sh

--- ol-app-install/1-build.sh
+++ ol-app-install/1-build.sh
@@ -13,42 +13,20 @@ oc login $OPENSHIFT_API_URL \
     --insecure-skip-tls-verify=true
 
 echo "Logging into Openshift image registry"
-if [ ! -z $USE_DOCKER ]
-then
-  docker login \
-      --username $OPENSHIFT_USERNAME \
-      --password $(oc whoami -t) \
-      $OPENSHIFT_REGISTRY_URL
-else
-  podman login \
-      --username $OPENSHIFT_USERNAME \
-      --password $(oc whoami -t) \
-      --tls-verify=false \
-      $OPENSHIFT_REGISTRY_URL
-fi
+podman login \
+    --username $OPENSHIFT_USERNAME \
+    --password $(oc whoami -t) \
+    --tls-verify=false \
+    $OPENSHIFT_REGISTRY_URL
 
 echo "Creating $OPENSHIFT_PROJECT project"
 oc new-project $OPENSHIFT_PROJECT
 
 echo "Building the container image"
-if [ ! -z $USE_DOCKER ]
-then
-  docker build \
-    -t ${OPENSHIFT_REGISTRY_URL}/$OPENSHIFT_PROJECT/app-modernization:v1.0.0 \
+buildah build-using-dockerfile \
+  -t ${OPENSHIFT_REGISTRY_URL}/$OPENSHIFT_PROJECT/app-modernization:v1.0.0 \
     .
-else
-  buildah build-using-dockerfile \
-    -t ${OPENSHIFT_REGISTRY_URL}/$OPENSHIFT_PROJECT/app-modernization:v1.0.0 \
-    .
-fi
-
+    
 echo "Pushing the container image to the Openshift image registrry"
-if [ ! -z $USE_DOCKER ]
-then
-  docker push \
-    ${OPENSHIFT_REGISTRY_URL}/${OPENSHIFT_PROJECT}/app-modernization:v1.0.0
-else
-  podman push --tls-verify=false \
-    ${OPENSHIFT_REGISTRY_URL}/${OPENSHIFT_PROJECT}/app-modernization:v1.0.0
-fi
-
+podman push --tls-verify=false \
+  ${OPENSHIFT_REGISTRY_URL}/${OPENSHIFT_PROJECT}/app-modernization:v1.0.0
